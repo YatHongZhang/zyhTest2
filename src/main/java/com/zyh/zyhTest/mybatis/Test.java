72cc93096520a2b206003afe39b1af88d8b94ace
zhangyihang <5974381@qq.com>
2018-10-12 15:38:07 +0800

    mybatis

---
+++ src/main/java/com/zyh/zyhTest/mybatis/Test.java
@@ -0,0 +1,32 @@
+package com.zyh.zyhTest.mybatis;
+
+import com.zyh.zyhTest.mybatis.dao.TestUserMapper;
+import com.zyh.zyhTest.mybatis.entity.TestUser;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * @author YatHong on 2018/10/11/0011
+ */
+public class Test {
+    public static SqlSession getSqlSession() throws FileNotFoundException {
+        //配置文件
+        InputStream configFile = new FileInputStream(
+                "E:\\projects\\zyhTest\\src\\main\\resources\\mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configFile);
+        //加载配置文件得到SqlSessionFactory
+        return sqlSessionFactory.openSession();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        TestUserMapper mapper = getSqlSession().getMapper(TestUserMapper.class);
+        TestUser user = mapper.selectByPrimaryKey(1);
+        System.out.println(user);
+    }
+
+}
---
+++ src/main/java/com/zyh/zyhTest/mybatis/dao/TestUserMapper.java
@@ -0,0 +1,17 @@
+package com.zyh.zyhTest.mybatis.dao;
+
+import com.zyh.zyhTest.mybatis.entity.TestUser;
+
+public interface TestUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TestUser record);
+
+    int insertSelective(TestUser record);
+
+    TestUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(TestUser record);
+
+    int updateByPrimaryKey(TestUser record);
+}

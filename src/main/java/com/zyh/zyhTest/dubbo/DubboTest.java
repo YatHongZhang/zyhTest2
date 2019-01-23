068ab6aa2a3e1e9be6d927b20dfeefc39056e077
zhangyihang <5974381@qq.com>
2019-01-23 17:21:58 +0800

    dubbo

--- src/main/java/com/zyh/zyhTest/dubbo/DubboTest.java
+++ src/main/java/com/zyh/zyhTest/dubbo/DubboTest.java
@@ -3,13 +3,30 @@ package com.zyh.zyhTest.dubbo;
 import com.alibaba.dubbo.common.extension.ExtensionLoader;
 import com.alibaba.dubbo.rpc.Protocol;
 
+import java.util.regex.Pattern;
+
 /**
  * @author YatHong on 2019/1/21/0021
  */
 public class DubboTest {
     public static void main(String[] args) {
 
-        String str = ExtensionLoader.getExtensionLoader(Protocol.class).getDefaultExtensionName();
+
+
+        //ExtensionLoader.getExtensionLoader(Protocol.class).getActivateExtension()
+        String str;
+        /*
+        str = ExtensionLoader.getExtensionLoader(Protocol.class).getDefaultExtensionName();
         System.out.println(str);
+        int port = ExtensionLoader.getExtensionLoader(Protocol.class).getDefaultExtension().getDefaultPort();
+        System.out.println(port);*/
+        str = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension().getClass().getName();
+        System.out.println(str);
+
+        String test = "h,hh";
+        String[] tests = Pattern.compile("\\s*[,]+\\s*").split(test);
+        for(String aaa:tests){
+            System.out.println(aaa);
+        }
     }
 }
